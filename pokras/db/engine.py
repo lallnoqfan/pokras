f46bd85f3a4f3d6ee0393b78db63cb15946ccd3f
lallnoqfan <lallnoqfan@gmail.com>
2025-08-08 01:55:26 +0300

    db: sqlite fk support

--- pokras/db/engine.py
+++ pokras/db/engine.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event, Engine
 from sqlalchemy.orm import sessionmaker
 
 from config import AppConfig, Paths
@@ -7,3 +7,20 @@ from config import AppConfig, Paths
 engine = create_engine(f"sqlite:///{Paths.SQLITE_DB}", echo=AppConfig.DEBUG)
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    # https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#foreign-key-support
+
+    # the sqlite3 driver will not set PRAGMA foreign_keys
+    # if autocommit=False; set to True temporarily
+    ac = dbapi_connection.autocommit
+    dbapi_connection.autocommit = True
+
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+    # restore previous autocommit setting
+    dbapi_connection.autocommit = ac
