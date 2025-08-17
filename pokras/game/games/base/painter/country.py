6948bab911b7a55c8d24a1c832211f6602dae42b
lallnoqfan <lallnoqfan@gmail.com>
2025-08-17 14:02:56 +0300

    fix: accidentally lost country model...

---
+++ pokras/game/games/base/painter/country.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Country:
+    """
+    Моделька страны, используемая для рендера карты и легенды.
+
+    Attributes:
+        name: Название страны
+        hex_color: Цвет страны в HEX формате
+        tiles: Список кодов тайлов, которые принадлежат данной стране
+    """
+    name: str
+    hex_color: str
+    tiles: list[str] = field(default_factory=lambda: [])
