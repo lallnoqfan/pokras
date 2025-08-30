68d866ab0a576130a0a3d6db34d848460701e387
lallnoqfan <lallnoqfan@gmail.com>
2025-08-31 00:40:55 +0300

    minor: update test cog

--- pokras/modules/tests/views.py
+++ pokras/modules/tests/views.py
@@ -2,7 +2,8 @@ from discord.ext.commands import Cog, group, Context, command
 
 from modules.game.queries.get_game import get_active_game_by_channel_id
 from modules.game.views import GameCommands
-from utils.perf import time_async
+from utils.checks import is_admin
+from utils.perf import time_async, logtime_async
 from utils.text import Tags as T
 
 
@@ -25,45 +26,57 @@ class TestCommands(Cog):
         if ctx.invoked_subcommand is None:
             await ctx.send_help("test")
 
-    @test.command(name="game")
-    async def test_game(self, ctx: Context):
+    async def __test_game(self, ctx: Context):
         game_cog = GameCommands(self.bot)
         initial_game = get_active_game_by_channel_id(ctx.channel.id)
         invoke = time_async(ctx.invoke)
+        total_time = 0
 
         if initial_game is not None:
             await ctx.send(f"{T.code('0/5')} invoking stop_game")
             _, time = await invoke(game_cog.stop_game, ctx)
+            total_time += time
             await ctx.send(T.code(f"{round(time, 4)}s"))
 
         await ctx.send(f"{T.code('1/5')} invoking list_games")
         _, time = await invoke(game_cog.list_games, ctx)
+        total_time += time
         await ctx.send(T.code(f"{round(time, 4)}s"))
 
         await ctx.send(f"{T.code('2/5')} invoking create_game")
         _, time = await invoke(game_cog.create_game, ctx)
+        total_time += time
         await ctx.send(T.code(f"{round(time, 4)}s"))
 
         new_game = get_active_game_by_channel_id(ctx.channel.id)
         await ctx.send(f"{T.code('3/5')} invoking stop_game")
         _, time = await invoke(game_cog.stop_game, ctx)
+        total_time += time
         await ctx.send(T.code(f"{round(time, 4)}s"))
 
         await ctx.send(f"{T.code('4/5')} invoking start_game")
         _, time = await invoke(game_cog.start_game, ctx, new_game.id)
+        total_time += time
         await ctx.send(T.code(f"{round(time, 4)}s"))
 
         await ctx.send(f"{T.code('5/5')} invoking stop_game")
         _, time = await invoke(game_cog.stop_game, ctx)
+        total_time += time
         await ctx.send(T.code(f"{round(time, 4)}s"))
 
         if initial_game is not None:
             await ctx.send(f"{T.code('6/5')} invoking start_game")
             _, time = await invoke(game_cog.start_game, ctx, initial_game.id)
+            total_time += time
             await ctx.send(T.code(f"{round(time, 4)}s"))
 
-        await ctx.send("show is over")
+        await ctx.send(f"pure total: {T.code(f'{round(total_time, 4)}s')}")
 
-    @test.command(name="roll")
-    async def game_tests(self, ctx: Context):
-        ...
+    @test.command(name="game")
+    @is_admin()
+    @logtime_async
+    async def test_game(self, ctx: Context):
+        await ctx.send(T.code("show time"))
+        _, time = await time_async(self.__test_game)(ctx)
+        await ctx.send(f"total: {T.code(f'{round(time, 4)}s')}")
+        await ctx.send(T.code("show is over"))
--- pokras/utils/perf.py
+++ pokras/utils/perf.py
@@ -35,3 +35,18 @@ def time_async(coro):
         execution_time = end_time - start_time
         return result, execution_time
     return wrapper
+
+
+def logtime_async(coro):
+    @wraps(coro)
+    async def wrapper(*args, **kwargs):
+        start_time = perf_counter()
+        result = await coro(*args, **kwargs)
+        end_time = perf_counter()
+        execution_time = end_time - start_time
+        print('=' * 50)
+        print(f"Переданные аргументы: {args}")
+        print(f"{coro.__name__} выполнилась за {execution_time:.4f} с.")
+        print('=' * 50)
+        return result
+    return wrapper
